24140074a0983e4ecc96645507de38c49e5c86e2
Somika Gouda <somikagouda@gmail.com>
2025-06-09 03:11:51 +0530

    Fix NLTK lookup error with custom download path

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
-nltk.download('punkt_tab')
-nltk.download('stopwords')
-from nltk.stem.porter import PorterStemmer
+import os
+nltk_data_path = os.path.join(os.path.dirname(__file__), 'nltk_data')
+os.makedirs(nltk_data_path, exist_ok=True)
+nltk.data.path.append(nltk_data_path)
+nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('stopwords', download_dir=nltk_data_path)
 from nltk.tokenize import word_tokenize
 
 ps = PorterStemmer()
