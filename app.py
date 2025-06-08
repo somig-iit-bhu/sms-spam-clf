b8f82ec3dc5acc7d98b219f4560a032da311bae1
Somika Gouda <somikagouda@gmail.com>
2025-06-09 03:16:18 +0530

    Initial commit

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ os.makedirs(nltk_data_path, exist_ok=True)
 nltk.data.path.append(nltk_data_path)
 nltk.download('punkt', download_dir=nltk_data_path)
 nltk.download('stopwords', download_dir=nltk_data_path)
+
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import word_tokenize
 
 ps = PorterStemmer()
